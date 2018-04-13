091e736829b1c00cf79f15915376b964edfa6ef2
Louis Bruge <louis.bruge@gmail.com>
2018-04-13 15:35:10 +0200

    Fix cut

--- exercises/cut.js
+++ exercises/cut.js
@@ -9,9 +9,25 @@
  * @notions String methods
  * https://github.com/nan-academy/refs/blob/master/js-training/methods.md#string---transform
  */
+let cutFirst = (n) => {
+  return n.slice(2, n.length)
+}
+
+let cutLast = (n) => {
+  return n.slice(0, (n.length - 2))
+}
+
+let cutFirstLast = (n) => {
+  return n.slice(2, (n.length - 2))
+}
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.strictEqual(typeof cutFirst, 'function')
+assert.strictEqual(typeof cutLast, 'function')
+assert.strictEqual(typeof cutFirstLast, 'function')
+assert.strictEqual(cutFirst('Salut tout le monde'), 'lut tout le monde')
+assert.strictEqual(cutLast('Salut tout le monde'), 'Salut tout le mon')
+assert.strictEqual(cutFirstLast('Salut tout le monde'), 'lut tout le mon')
 // End of tests */
